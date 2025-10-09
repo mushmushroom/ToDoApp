d5a5a0a3d3afa2fb0519cec3d8481d3a0002535c
Maria Smirnitskaya <smirnitskayamp@gmail.com>
2025-10-09 12:05:14 +0300

    Add toast on login for google and github

--- src/components/auth/LoginWithGithub.tsx
+++ src/components/auth/LoginWithGithub.tsx
@@ -1,17 +1,27 @@
 import { FaGithub } from 'react-icons/fa';
 import { Button } from '../ui/button';
 import { signIn } from 'next-auth/react';
+import { toast } from 'sonner';
+import { useRouter } from 'next/navigation';
 
 export default function LoginWithGithub() {
+  const router = useRouter();
 
-  function onSignInWithGithub() {
-    signIn('github', { callbackUrl: '/my-tasks', redirect: true });
-    
+  async function handleGithubLogin() {
+    const result = await signIn('github', { redirect: false, callbackUrl: '/my-tasks' });
+
+    if (result?.error) {
+      toast.error(result.error);
+    } else {
+      toast.success('Logged in successfully!');
+      router.push(result?.url || '/my-tasks');
+    }
   }
+
   return (
     <Button
       type="button"
-      onClick={() => signIn('github', { callbackUrl: '/my-tasks', redirect: true })}
+      onClick={handleGithubLogin}
       variant="secondary"
       className="w-full cursor-pointer"
     >
--- src/components/auth/LoginWithGoogle.tsx
+++ src/components/auth/LoginWithGoogle.tsx
@@ -1,12 +1,26 @@
 import { Button } from '../ui/button';
 import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 import { FaGoogle } from 'react-icons/fa';
+import { toast } from 'sonner';
 
 export default function LoginWithGoogle() {
+  const router = useRouter();
+
+  async function handleGithubLogin() {
+    const result = await signIn('google', { redirect: false, callbackUrl: '/my-tasks' });
+
+    if (result?.error) {
+      toast.error(result.error);
+    } else {
+      toast.success('Logged in successfully!');
+      router.push(result?.url || '/my-tasks');
+    }
+  }
   return (
     <Button
       type="button"
-      onClick={() => signIn('google', { callbackUrl: '/my-tasks', redirect: true })}
+      onClick={handleGithubLogin}
       variant="secondary"
       className="w-full cursor-pointer"
     >
