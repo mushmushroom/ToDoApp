7dcc31ead6bfd963c9e0ec003754a9371a099eca
Maria Smirnitskaya <smirnitskayamp@gmail.com>
2025-10-12 17:53:03 +0300

    Change middleware to fit the limit

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,18 @@
-// export { auth as middleware } from '@/config/auth';
-
-import { auth } from '@/lib/config/auth';
 import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-export default auth((req) => {
-  if (!req.auth) {
+export function middleware(req: NextRequest) {
+  const token =
+    req.cookies.get('next-auth.session-token')?.value ||
+    req.cookies.get('__Secure-next-auth.session-token')?.value;
+
+  if (!token) {
     const signInUrl = new URL('/auth/sign-in', req.url);
     return NextResponse.redirect(signInUrl);
   }
 
   return NextResponse.next();
-});
+}
 
 export const config = {
   matcher: ['/my-tasks/:path*', '/settings/:path*'],
