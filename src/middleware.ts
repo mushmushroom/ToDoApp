444f53c1da159d8225e3e5f78d379d080299b31d
Maria Smirnitskaya <smirnitskayamp@gmail.com>
2025-10-13 10:33:19 +0300

    Update middleware to redirect correctly

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,11 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(req: NextRequest) {
-  const token =
-    req.cookies.get('next-auth.session-token')?.value ||
-    req.cookies.get('__Secure-next-auth.session-token')?.value;
+  const isAuthTokenPresent = (req: NextRequest): boolean =>
+    !!req.cookies.get('next-auth.session-token')?.value ||
+    !!req.cookies.get('__Secure-next-auth.session-token')?.value;
 
-  if (!token) {
+  if (!isAuthTokenPresent) {
     const signInUrl = new URL('/auth/sign-in', req.url);
     return NextResponse.redirect(signInUrl);
   }
