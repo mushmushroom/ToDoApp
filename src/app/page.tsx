31706311f115ea08ad51df49e4a78933de68d474
Maria Smirnitskaya <smirnitskayamp@gmail.com>
2025-10-28 13:24:20 +0200

    Remove demo link from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,10 +28,10 @@ export default async function Home() {
                 <Link href={AppPath.SignIn}>Log in</Link>
               </Button>
               <div>
-                or view
+                {/* or view
                 <Button variant="link" asChild>
                   <Link href={AppPath.Demo}>Live Demo</Link>
-                </Button>
+                </Button> */}
               </div>
             </div>
           )}
